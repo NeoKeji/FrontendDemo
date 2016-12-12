e8aa146cce5de1a7f54b2252f7df77388e8535b2
Bailin <libailin1989@hotmail.com>
2016-12-12 14:13:26 +0800

    init new branch by Bailin, convert 3dview to object oriented

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import Nav from '../components/Nav.jsx';
-import Model3D from '../components/Model3D.jsx';
+import View3D from '../components/View3D.jsx';
 import Panel from '../components/Panel.jsx';
 import Tabs from '../components/Tabs.jsx';
 
-// 
+//
 
 class App extends React.Component {
   constructor(props){
     super(props);
-    
+
   }
   render() {
     return (
       <div>
         <Nav/>
-         <Model3D/>
         <Panel content={Tabs}/>
+        <View3D/>
       </div>
     );
   }
